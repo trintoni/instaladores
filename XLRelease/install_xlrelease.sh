3cdfb1023f28c92e95a888b3ec977c5b54164e7a
Trinta <trintoni@gmail.com>
2022-11-30 12:24:12 -0300

    Adding script installer XLRELEASE

---
+++ XLRelease/install_xlrelease.sh
@@ -0,0 +1,14 @@
+which kubectl
+if [ $? = 1 ];then
+    echo "Por Favor instale o kubectl"
+fi
+echo "Habilitando o ingress ao minikube"
+minikube addons enable ingress
+echo "Criando o namespace xlrelease"
+kubectl namespace xlrelease
+echo "Criando o deploy"
+kubectl apply -f deployxl.yaml -n xlrelease
+echo "Criando o services"
+kubectl apply -f services.yaml -n xlrelease
+echo "Criando o Ingress"
+kubectl apply -f ingress.yaml -n xlrelease
