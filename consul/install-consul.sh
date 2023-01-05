7b25cd7c89528bdb6cb85b07e530e7a188438a03
Trintoni <trintoni@gmail.com>
2023-01-05 15:36:57 +0000

    fix access consul

--- consul/install-consul.sh
+++ consul/install-consul.sh
@@ -1,5 +1,9 @@
 helm repo add hashicorp https://helm.releases.hashicorp.com
 kubectl create namespace consul
 helm install consul hashicorp/consul --set global.name=consul --create-namespace --namespace consul
+sleep 200
 # Habilitando acesso remoto com port foward
-kubectl port-forward service/consul-server --namespace consul 8500:8500
+kubectl port-forward svc/consul-ui --namespace consul 8501:443 --address='0.0.0.0' &
+
+# SITE
+# https://developer.hashicorp.com/consul/tutorials/kubernetes/kubernetes-minikube?utm_source=docs
